d899d69d52be6d9552d2a5d4082289508c7473b4
Вахрушева Полина Андреевна <98634514+Jinamy2@users.noreply.github.com>
2022-12-01 12:25:33 +0300

    Update PRSAM2.py

--- PRSAM2.py
+++ PRSAM2.py
@@ -1,8 +1,11 @@
 import pymysql
-connection = pymysql.connect(host='5.183.188.132',user="db_vgu_student",password="thasrCt3pKYWAYcK",database="db_vgu_test")
+connection = pymysql.connect(host='5.183.188.132',
+                             user="db_vgu_student",
+                             password="thasrCt3pKYWAYcK",
+                             database="db_vgu_test")
 with connection:
     cur = connection.cursor()
-    cur.execute("SELECT * FROM category")
+    cur.execute("SELECT * FROM `language`")
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        print(row ["locale"])
